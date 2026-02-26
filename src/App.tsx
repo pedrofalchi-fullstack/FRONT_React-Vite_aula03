81bc2c780b154f024b4cdba624bcef33f349cddc
pedrofalchi-fullstack <peeh.falchi@gmail.com>
2026-02-26 13:55:48 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () =>{
+  return(
+    <>
+      <header>Meu site</header>
+      <main>
+        <p className="xyz">Bem vindo a minha primeira aplicação com CSS Global.</p>
+      </main>
+    </>
+  )
+}
+export default App;
